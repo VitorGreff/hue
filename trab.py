0f9937a9e3b919eb07aaa6a18c8031d7fb76bca6
VitorGreff <vitorgreffdias@gmail.com>
2023-09-20 17:45:03 -0300

    managed to fix red transitions

--- trab.py
+++ trab.py
@@ -15,11 +15,11 @@ def plot_results(before, after):
 # a -> imagem, m -> intervalo, x -> variação
 def alterar_faixa_matizes(a, m, x):
     imagem_hsv = cv2.cvtColor(a, cv2.COLOR_BGR2HSV)
-
+    imagem_hsv = imagem_hsv.astype(np.uint16)
     matiz_max = np.mod((m + x) / 2, 180)
     matiz_min = np.mod((m - x) / 2, 180)
 
-    print(f"[Min] {matiz_min} <--> {matiz_max} [Max]")
+    print(f"[Min] {matiz_min*2} <--> {matiz_max*2} [Max]")
 
     if matiz_min < matiz_max:
         # Não precisa de ajustes circulares, pois matiz_min é menor que matiz_max
@@ -28,9 +28,6 @@ def alterar_faixa_matizes(a, m, x):
             (imagem_hsv[:, :, 0] + 90) % 180,
             imagem_hsv[:, :, 0]
         )
-        # imagem_rgb = cv2.cvtColor(imagem_hsv, cv2.COLOR_HSV2RGB)
-        # plot_results(a, imagem_rgb)
-
     elif matiz_min > matiz_max:
         # Ajuste circular para o caso em que matiz_min é maior que matiz_max
         imagem_hsv[:, :, 0] = np.where(
@@ -39,26 +36,26 @@ def alterar_faixa_matizes(a, m, x):
             (imagem_hsv[:, :, 0] + 90) % 180,
             imagem_hsv[:, :, 0]
         )
-        
+
+    imagem_hsv = imagem_hsv.astype(np.uint8)
     imagem_rgb = cv2.cvtColor(imagem_hsv, cv2.COLOR_HSV2RGB)
     plot_results(a, imagem_rgb)
 
 def main():
-    imagem_entrada = cv2.imread("circulo_cromatico.jpeg")  
-    alterar_faixa_matizes(imagem_entrada, 210, 45)
-
-    imagem_entrada = cv2.imread("circulo.jpg")  
-    alterar_faixa_matizes(imagem_entrada, 30, 60)
+    imagem_entrada = cv2.imread("circulo.jpg") 
+    alterar_faixa_matizes(imagem_entrada, 20, 38)
 
     imagem_entrada = cv2.imread("circulo2.png")  
-    alterar_faixa_matizes(imagem_entrada, 30, 60)
+    alterar_faixa_matizes(imagem_entrada, 30, 70)
 
-    imagem_entrada = cv2.imread("circulo3.png")  
-    alterar_faixa_matizes(imagem_entrada, 30, 60)
+    imagem_entrada = cv2.imread("museum.jpg")  
+    alterar_faixa_matizes(imagem_entrada, 20, 38)
 
-    # imagem_entrada = cv2.imread("aves.jpg")  
-    # alterar_faixa_matizes(imagem_entrada, 30, 60)
+    imagem_entrada = cv2.imread("balloon.jpg")  
+    alterar_faixa_matizes(imagem_entrada, 60, 30)
 
+    imagem_entrada = cv2.imread("aves.jpg")  
+    alterar_faixa_matizes(imagem_entrada, 20, 38)
 
 if __name__ == "__main__":
     main()
